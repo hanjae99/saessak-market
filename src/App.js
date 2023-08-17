bb28ec575b3bae3f87dcc53e66a26460bbcf6ae0
Koo612 <ksm99007@gmail.com>
2023-08-17 22:24:49 +0900

    test02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      // 코딩 천재 이한재
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
