81086173a193dcfe17a935e4d4667d803d43c113
kimfanmi <kkhpeoplez@gmail.com>
2023-08-21 18:47:20 +0900

    오류수정

--- src/components/admin/AdminEdit.jsx
+++ src/components/admin/AdminEdit.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const EditBody = ({ page }) => {
   const pitem = useSelector(state => state.adminData.select_pitem);
   const pboard = useSelector(state => state.product)
   const fboard = useSelector(state => state.board)
   const data = page === 'productboard' ? pboard : fboard;
+  const dispatch = useDispatch();
   let interval = [];
   let kd = false;
   return (
-    <div style={{ position: 'relative', display: 'flex' }}>
-      <button style={{ position: 'relative', top: '50%', padding: '11px', fontSize: '1.2rem' }}
+    <div style={{ position: 'relative', display: 'flex', height:'100px' }}>
+      <button style={{ position: 'relative', padding: '11px', fontSize: '1.2rem' }}
         onMouseDown={(e) => {
           kd = true;
           if (parseInt(e.target.nextElementSibling.children[0].style.marginLeft) < 15 - 100)
             e.target.nextElementSibling.children[0].style.marginLeft = parseInt(e.target.nextElementSibling.children[0].style.marginLeft) + 100 + 'px';
           setTimeout(() => {
             if (kd) {
-              console.log(interval);
               interval.push(setInterval(() => {
                 if (parseInt(e.target.nextElementSibling.children[0].style.marginLeft) < 15)
-                  e.target.nextElementSibling.children[0].style.marginLeft = parseInt(e.target.nextElementSibling.children[0].style.marginLeft) + 4 + 'px';
+                  e.target.nextElementSibling.children[0].style.marginLeft = parseInt(e.target.nextElementSibling.children[0].style.marginLeft) + 6 + 'px';
               }, 10))
             }
           }, 500)
@@ -31,10 +31,13 @@ const EditBody = ({ page }) => {
             clearInterval(interval.pop());
           }
         }} >{'<'}</button>
-      <div style={{ display: 'flex', width: '700px', position: 'relative', overflow: 'hidden' }}>
-        {pitem && pitem.map(p => <div key={p} style={{ marginLeft: '15px' }}>{data.map(q => q.id === p ? <img key={q.id} style={{ width: '80px', height: '80px' }} src={q.imgsrc1} alt=''></img> : '')}</div>)}
+      <div style={{ display: 'flex', width: '700px', position: 'relative', overflow: 'hidden', alignItems:'center' }}>
+        {pitem && pitem.map(p => <div key={p} style={{ marginLeft: '15px', height:'80px' }} onContextMenu={e=>{
+          e.preventDefault();
+          dispatch({type:'adminData/delSP',payload:p});
+        }} >{data.map(q => q.id === p ? <img key={q.id} style={{ width: '80px', height: '80px' }} src={q.imgsrc1} alt=''></img> : '')}</div>)}
       </div>
-      <button style={{ position: 'relative', right: '0px', top: '50%', padding: '11px', fontSize: '1.2rem' }}
+      <button style={{ position: 'relative', right: '0px', padding: '11px', fontSize: '1.2rem' }}
         onMouseDown={(e) => {
           kd = true;
           if (parseInt(e.target.previousElementSibling.children[0].style.marginLeft) > 840 - e.target.previousElementSibling.children.length * 100)
@@ -43,7 +46,7 @@ const EditBody = ({ page }) => {
             if (kd) {
               interval.push(setInterval(() => {
                 if (parseInt(e.target.previousElementSibling.children[0].style.marginLeft) > 740 - e.target.previousElementSibling.children.length * 100)
-                  e.target.previousElementSibling.children[0].style.marginLeft = parseInt(e.target.previousElementSibling.children[0].style.marginLeft) - 4 + 'px';
+                  e.target.previousElementSibling.children[0].style.marginLeft = parseInt(e.target.previousElementSibling.children[0].style.marginLeft) - 6 + 'px';
               }, 10))
             }
           }, 500)
