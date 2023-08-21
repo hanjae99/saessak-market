30d3f460dda01f97ebb200c0e06058d830cc4cfe
Koo612 <ksm99007@gmail.com>
2023-08-21 13:48:30 +0900

    realnotice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { Provider } from 'react-redux';
 import './App.css';
-
+import NoticeBoardList from './components/NoticeBoardList';
+import NoticeBoardSelect from './components/NoticeBoardSelect';
+import store from './store';
 function App() {
   return (
-    <div className="App">
-      // 코딩 천재 이한재
-      <div>
-        <p></p>
+    <Provider store={store}>
+      <div className="container">
+        <div className="header">
+          <h2>nav바</h2>
+        </div>
+        <aside className="left-menu">
+          <NoticeBoardList />
+        </aside>
+        <section>
+          <NoticeBoardSelect />
+        </section>
+        <aside className="right-menu"></aside>
       </div>
-    </div>
+    </Provider>
   );
 }
 
