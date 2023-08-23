e0645a665410a7bcb87a5b7fb04e93efbab1e1be
Koo612 <ksm99007@gmail.com>
2023-08-23 15:56:57 +0900

    0823

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import NoticeBoardList from './components/NoticeBoardList';
-import NoticeBoardSelect from './components/NoticeBoardSelect';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import AdminPage from './components/admin/AdminPage';
+import BoardMain from './components/board/BoardMain';
+import CreateNotice from './components/board/CreateNotice';
+import CreateVoice from './components/board/CreateVoice';
+import { useState } from 'react';
+import BoardInfo from './components/board/BoardInfo';
 
 function App() {
+  const [page, setPage] = useState(1);
   return (
-    <>
+    <div>
       <div className="container">
         <div className="header">
           <h2>nav바</h2>
         </div>
-        <aside className="left-menu">
-          <NoticeBoardList />
-        </aside>
-        <section>
-          <NoticeBoardSelect />
-        </section>
-        <aside className="right-menu"></aside>
-      </div>
-      <div>
+        <BoardInfo />
         <Routes>
           <Route path="/admin/:page?" element={<AdminPage />} />
+          <Route path="/boardmain" element={<BoardMain page={page} />} />
+          <Route path="/boardwrite" element={<CreateNotice />} />
+          <Route path="/boardmain/1" element={<CreateVoice page={page} />} />
           <Route path="/*" />
         </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
