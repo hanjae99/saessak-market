f0430dea033974cb7b4c3795bed911b01e73ca81
kimfanmi <kkhpeoplez@gmail.com>
2023-08-25 11:15:34 +0900

    병합오류수정

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -23,7 +23,7 @@ const Detail = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="detail-container">
+    <main className="detail-container">
       <Header />
       <main className="detail-contentsBox">
         <div className="detail-contentsBox2">
@@ -104,8 +104,8 @@ const Detail = () => {
             )}
           </div>
         </div>
-      </div>
-    </div>
+      </main>
+    </main>
   );
 };
 
