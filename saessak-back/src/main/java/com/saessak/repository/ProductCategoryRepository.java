45afdb13e7d844092a55f96489a5290557dcb229
SANGHYEON <ppsh0703@daum.net>
2023-10-20 17:45:15 +0900

    아직 상세페이지 하는중

--- saessak-back/src/main/java/com/saessak/repository/ProductCategoryRepository.java
+++ saessak-back/src/main/java/com/saessak/repository/ProductCategoryRepository.java
@@ -1,9 +1,15 @@
 package com.saessak.repository;
 
 import com.saessak.entity.Product;
+import com.saessak.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProductCategoryRepository extends JpaRepository<Product,Long> {
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
+
+
+    List<ProductCategory> findByProductId(Long productId);
 }
