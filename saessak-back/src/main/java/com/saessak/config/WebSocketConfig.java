4f0b54386c53f05558ae1bfe5d5ac435167e7b07
sang hyeon <ppsh0703@daum.net>
2023-10-28 17:55:33 +0900

    웹소켓 아직하는중

--- saessak-back/src/main/java/com/saessak/config/WebSocketConfig.java
+++ saessak-back/src/main/java/com/saessak/config/WebSocketConfig.java
@@ -12,13 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");
-        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+        registry.addEndpoint("/chatting").setAllowedOrigins("*")
                 .withSockJS();
     }
 }
