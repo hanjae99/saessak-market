47838548a322dccb0108c5ef380258a9cc13c96a
userkimjin <rlawls2006@naver.com>
2023-11-02 23:21:45 +0900

    페이징 처리 완료

--- saessak-front/src/components/kimjin/Check.jsx
+++ saessak-front/src/components/kimjin/Check.jsx
@@ -7,9 +7,10 @@ import { useEffect } from "react";
 import { call } from "../../ApiService";
 import { useState } from "react";
 import { API_BASE_URL } from "../../ApiConfig";
+import { useCallback } from "react";
 
 const Check = () => {
-  const movePage = useNavigate();
+  const movePages = useNavigate();
 
   const [buyProduct, setBuyProduct] = useState([]);
 
@@ -20,68 +21,126 @@ const Check = () => {
     });
   }, []);
 
+  // 페이징 처리(한 페이지당 30개의 상품 노출)
+  const [pageBtns, setPageBtns] = useState([]); // 페이지 버튼들을 저장하는 상태 변수
+  const [pageNumLength, setpageNumLength] = useState(0);
+
+  // 페이지 이동 처리 함수
+  const movePage = useCallback((i) => {
+    setpageNumLength(i - 1); // 선택된 페이지로 pageNumLength 설정
+    window.scrollTo(0, 0); // 페이지 이동 후 맨 위로 스크롤
+  }, []);
+
+  useEffect(() => {
+    // 총 페이지 수 계산
+
+    const makePageBtn = () => {
+      const totalPage = Math.ceil(buyProduct.length / 2);
+
+      const newPageBtns = [];
+      // 페이지 버튼 6개씩 보여주기
+      for (
+        let i = 1 + 5 * pageNumLength;
+        i <= 1 + 5 * (pageNumLength + 1);
+        i++
+      ) {
+        if (i > totalPage) {
+          break;
+        }
+        const pageBtn = (
+          <button onClick={() => movePage(i)} key={i}>
+            {i}
+          </button>
+        );
+        newPageBtns.push(pageBtn);
+      }
+      setPageBtns(newPageBtns); // 페이지 버튼 상태 업데이트
+    };
+    makePageBtn(); // 페이지 버튼 생성 함수 호출
+  }, [buyProduct, movePage]);
+
+  console.log(pageNumLength);
+  console.log("안녕", pageBtns);
+
+  // page 버튼 6개씩 보여주기
+  const prevPageNumLength = useCallback(() => {
+    setpageNumLength(pageNumLength - 1); // 이전 페이지 세트로 이동
+  }, [pageNumLength]);
+
+  const nextPageNumLength = useCallback(() => {
+    setpageNumLength(pageNumLength + 1); // 다음 페이지 세트로 이동
+  }, [pageNumLength]);
+
+  const itemsPerPage = 2; // 페이지당 상품 수
+  const displayedProducts = buyProduct.slice(
+    pageNumLength * itemsPerPage,
+    Math.min((pageNumLength + 1) * itemsPerPage, buyProduct.length)
+  );
+  console.log(displayedProducts);
+
   return (
     <div className="section">
       <div className="manu-2">
         <div className="manu-2-1">
           <div className="tbody-1">
             <div className="text-0">
-              {buyProduct &&
-                buyProduct.map((a, i) => (
-                  <div key={i}>
-                    <div className="table-main">
-                      <div className="table-body">
-                        <div className="table-day">
-                          {(() => {
-                            const date = new Date(buyProduct[i].updateTime);
-                            const year = date.getFullYear();
-                            const month = (1 + date.getMonth())
-                              .toString()
-                              .padStart(2, "0");
-                            const day = date
-                              .getDate()
-                              .toString()
-                              .padStart(2, "0");
-                            return `${year}-${month}-${day}`;
-                          })()}
-                        </div>
+              {displayedProducts.map((a, i) => (
+                <div key={i}>
+                  <div className="table-main">
+                    <div className="table-body">
+                      <div className="table-day">
+                        {(() => {
+                          const date = new Date(
+                            displayedProducts[i].updateTime
+                          );
+                          const year = date.getFullYear();
+                          const month = (1 + date.getMonth())
+                            .toString()
+                            .padStart(2, "0");
+                          const day = date
+                            .getDate()
+                            .toString()
+                            .padStart(2, "0");
+                          return `${year}-${month}-${day}`;
+                        })()}
                       </div>
-                      <div className="td-main">
-                        <div className="td-1">
-                          <div className="td-1-1">
-                            <div className="td-1-1-1">
-                              <span className="td-1-1-1-1">
-                                {buyProduct[i].sellStatus}
-                              </span>
-                            </div>
+                    </div>
+                    <div className="td-main">
+                      <div className="td-1">
+                        <div className="td-1-1">
+                          <div className="td-1-1-1">
+                            <span className="td-1-1-1-1">
+                              {displayedProducts[i].sellStatus}
+                            </span>
                           </div>
-                          <div>
-                            <div className="text-1">
-                              <div className="text-1-1">
-                                <div className="text-2">
-                                  <div
-                                    className="text-2-1"
-                                    onClick={() =>
-                                      movePage(
-                                        "/detail/" + buyProduct[i].productId
-                                      )
-                                    }
-                                  >
-                                    <div className="text-2-img">
-                                      <img
-                                        className="img1"
-                                        src={`${API_BASE_URL}${buyProduct[i].imgUrl}`}
-                                        alt=""
-                                      />
-                                    </div>
-                                    <div className="text-2-name">
-                                      <div className="text-2-name-1">
-                                        <div className="text-2-name-1-1">
-                                          {buyProduct[i].title}
-                                        </div>
-                                        <div className="text-2-name-1-2">
-                                          {buyProduct[i].price}
-                                        </div>
+                        </div>
+                        <div>
+                          <div className="text-1">
+                            <div className="text-1-1">
+                              <div className="text-2">
+                                <div
+                                  className="text-2-1"
+                                  onClick={() =>
+                                    movePages(
+                                      "/detail/" +
+                                        displayedProducts[i].productId
+                                    )
+                                  }
+                                >
+                                  <div className="text-2-img">
+                                    <img
+                                      className="img1"
+                                      src={`${API_BASE_URL}${displayedProducts[i].imgUrl}`}
+                                      alt=""
+                                    />
+                                  </div>
+                                  <div className="text-2-name">
+                                    <div className="text-2-name-1">
+                                      <div className="text-2-name-1-1">
+                                        {displayedProducts[i].title}
+                                      </div>
+                                      <div className="text-2-name-1-2">
+                                        {displayedProducts[i].price} 원
                                       </div>
                                     </div>
                                   </div>
@@ -90,36 +149,50 @@ const Check = () => {
                             </div>
                           </div>
                         </div>
-                        <div className="td-2">
-                          <div className="text-button">
-                            <Button
-                              onClick={() => {
-                                console.log(buyProduct[i].buyListId);
-                                call(
-                                  `/user/check/${buyProduct[i].buyListId}`,
-                                  "DELETE"
-                                ).then((response) => {
-                                  if (response.error === "success") {
-                                    const filteredWish = buyProduct.filter(
-                                      (w) =>
-                                        w.buyListId !== buyProduct[i].buyListId
-                                    );
-                                    setBuyProduct(filteredWish);
-                                  }
-                                });
-                              }}
-                              className="text-button-1"
-                            >
-                              상품삭제
-                            </Button>
-                          </div>
+                      </div>
+                      <div className="td-2">
+                        <div className="text-button">
+                          <Button
+                            onClick={() => {
+                              console.log(displayedProducts[i].buyListId);
+                              call(
+                                `/user/check/${displayedProducts[i].buyListId}`,
+                                "DELETE"
+                              ).then((response) => {
+                                if (response.error === "success") {
+                                  const filteredWish = displayedProducts.filter(
+                                    (w) =>
+                                      w.buyListId !==
+                                      displayedProducts[i].buyListId
+                                  );
+                                  setBuyProduct(filteredWish);
+                                }
+                              });
+                            }}
+                            className="text-button-1"
+                          >
+                            상품삭제
+                          </Button>
                         </div>
                       </div>
                     </div>
                   </div>
-                ))}
+                </div>
+              ))}
             </div>
           </div>
+        </div>
+        <div className="pagination">
+          <button onClick={prevPageNumLength} disabled={pageNumLength === 0}>
+            이전
+          </button>
+          {pageBtns}
+          <button
+            onClick={nextPageNumLength}
+            disabled={pageNumLength === Math.ceil(buyProduct.length / 2) - 1}
+          >
+            다음
+          </button>
         </div>
       </div>
     </div>
--- saessak-front/src/components/kimjin/WishList.jsx
+++ saessak-front/src/components/kimjin/WishList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import "./Manu.css";
@@ -8,7 +8,7 @@ import { API_BASE_URL } from "../../ApiConfig";
 import { Start } from "@mui/icons-material";
 
 const WishList = () => {
-  const movePage = useNavigate();
+  const movePages = useNavigate();
   const [wish, setWish] = useState([]);
 
   useEffect(() => {
@@ -26,65 +26,119 @@ const WishList = () => {
 
   console.log(wish);
 
+  // 페이징 처리(한 페이지당 30개의 상품 노출)
+  const [pageBtns, setPageBtns] = useState([]); // 페이지 버튼들을 저장하는 상태 변수
+  const [pageNumLength, setpageNumLength] = useState(0);
+
+  // 페이지 이동 처리 함수
+  const movePage = useCallback((i) => {
+    setpageNumLength(i - 1); // 선택된 페이지로 pageNumLength 설정
+    window.scrollTo(0, 0); // 페이지 이동 후 맨 위로 스크롤
+  }, []);
+
+  useEffect(() => {
+    // 총 페이지 수 계산
+
+    const makePageBtn = () => {
+      const totalPage = Math.ceil(wish.length / 2);
+
+      const newPageBtns = [];
+      // 페이지 버튼 6개씩 보여주기
+      for (
+        let i = 1 + 5 * pageNumLength;
+        i <= 1 + 5 * (pageNumLength + 1);
+        i++
+      ) {
+        if (i > totalPage) {
+          break;
+        }
+        const pageBtn = (
+          <button onClick={() => movePage(i)} key={i}>
+            {i}
+          </button>
+        );
+        newPageBtns.push(pageBtn);
+      }
+      setPageBtns(newPageBtns); // 페이지 버튼 상태 업데이트
+    };
+    makePageBtn(); // 페이지 버튼 생성 함수 호출
+  }, [wish, movePage]);
+
+  console.log(pageNumLength);
+  console.log("안녕", pageBtns);
+
+  // page 버튼 6개씩 보여주기
+  const prevPageNumLength = useCallback(() => {
+    setpageNumLength(pageNumLength - 1); // 이전 페이지 세트로 이동
+  }, [pageNumLength]);
+
+  const nextPageNumLength = useCallback(() => {
+    setpageNumLength(pageNumLength + 1); // 다음 페이지 세트로 이동
+  }, [pageNumLength]);
+
+  const itemsPerPage = 2; // 페이지당 상품 수
+  const displayedProducts = wish.slice(
+    pageNumLength * itemsPerPage,
+    Math.min((pageNumLength + 1) * itemsPerPage, wish.length)
+  );
+  console.log(displayedProducts);
+
   return (
     <div className="section">
       <div className="manu-2">
         <div className="manu-2-1">
           <div className="tbody-1">
-            {wish &&
-              wish.map((a, i) => (
-                <div key={i}>
-                  <div className="table-main">
-                    <div className="table-body">
-                      <div className="table-day">
-                        {(() => {
-                          const date = new Date(wish[i].updateTime);
-                          const year = date.getFullYear();
-                          const month = (1 + date.getMonth())
-                            .toString()
-                            .padStart(2, "0");
-                          const day = date
-                            .getDate()
-                            .toString()
-                            .padStart(2, "0");
-                          return `${year}-${month}-${day}`;
-                        })()}
-                      </div>
+            {displayedProducts.map((a, i) => (
+              <div key={i}>
+                <div className="table-main">
+                  <div className="table-body">
+                    <div className="table-day">
+                      {(() => {
+                        const date = new Date(displayedProducts[i].updateTime);
+                        const year = date.getFullYear();
+                        const month = (1 + date.getMonth())
+                          .toString()
+                          .padStart(2, "0");
+                        const day = date.getDate().toString().padStart(2, "0");
+                        return `${year}-${month}-${day}`;
+                      })()}
                     </div>
-                    <div className="td-main">
-                      <div className="td-1">
-                        <div className="td-1-1">
-                          <div className="td-1-1-1">
-                            <span className="td-1-1-1-1">
-                              {wish[i].sellStatus}
-                            </span>
-                          </div>
+                  </div>
+                  <div className="td-main">
+                    <div className="td-1">
+                      <div className="td-1-1">
+                        <div className="td-1-1-1">
+                          <span className="td-1-1-1-1">
+                            {displayedProducts[i].sellStatus}
+                          </span>
                         </div>
-                        <div>
-                          <div className="text-1">
-                            <div className="text-1-1">
-                              <div className="text-2">
-                                <div
-                                  className="text-2-1"
-                                  onClick={() =>
-                                    movePage("/detail/" + wish[i].productId)
-                                  }
-                                >
-                                  <div className="text-2-img">
-                                    <img
-                                      className="img1"
-                                      src={`${API_BASE_URL}${wish[i].imgUrl}`}
-                                      alt=""
-                                    />
-                                  </div>
-                                  <div className="text-2-name">
-                                    <div className="text-2-name-1">
-                                      <div className="text-2-name-1-1">
-                                        {wish[i].title}
-                                      </div>
-                                      <div className="text-2-name-1-2">
-                                        {wish[i].price}
-                                      </div>
+                      </div>
+                      <div>
+                        <div className="text-1">
+                          <div className="text-1-1">
+                            <div className="text-2">
+                              <div
+                                className="text-2-1"
+                                onClick={() =>
+                                  movePages(
+                                    "/detail/" + displayedProducts[i].productId
+                                  )
+                                }
+                              >
+                                <div className="text-2-img">
+                                  <img
+                                    className="img1"
+                                    src={`${API_BASE_URL}${displayedProducts[i].imgUrl}`}
+                                    alt=""
+                                  />
+                                </div>
+                                <div className="text-2-name">
+                                  <div className="text-2-name-1">
+                                    <div className="text-2-name-1-1">
+                                      {displayedProducts[i].title}
+                                    </div>
+                                    <div className="text-2-name-1-2">
+                                      {displayedProducts[i].price} 원
                                     </div>
                                   </div>
                                 </div>
@@ -93,34 +147,49 @@ const WishList = () => {
                           </div>
                         </div>
                       </div>
-                      <div className="td-2">
-                        <div className="text-button">
-                          <Button
-                            onClick={() => {
-                              console.log(wish[i].wishListId);
-                              call(
-                                `/user/wishlist/${wish[i].wishListId}`,
-                                "DELETE"
-                              ).then((response) => {
-                                if (response.error === "success") {
-                                  const filteredWish = wish.filter(
-                                    (w) => w.wishListId !== wish[i].wishListId
-                                  );
-                                  setWish(filteredWish);
-                                }
-                              });
-                            }}
-                            className="text-button-1"
-                          >
-                            상품삭제
-                          </Button>
-                        </div>
+                    </div>
+                    <div className="td-2">
+                      <div className="text-button">
+                        <Button
+                          onClick={() => {
+                            console.log(displayedProducts[i].wishListId);
+                            call(
+                              `/user/wishlist/${displayedProducts[i].wishListId}`,
+                              "DELETE"
+                            ).then((response) => {
+                              if (response.error === "success") {
+                                const filteredWish = displayedProducts.filter(
+                                  (w) =>
+                                    w.wishListId !==
+                                    displayedProducts[i].wishListId
+                                );
+                                setWish(filteredWish);
+                              }
+                            });
+                          }}
+                          className="text-button-1"
+                        >
+                          상품삭제
+                        </Button>
                       </div>
                     </div>
                   </div>
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
+        </div>
+        <div className="pagination">
+          <button onClick={prevPageNumLength} disabled={pageNumLength === 0}>
+            이전
+          </button>
+          {pageBtns}
+          <button
+            onClick={nextPageNumLength}
+            disabled={pageNumLength === Math.ceil(wish.length / 2) - 1}
+          >
+            다음
+          </button>
         </div>
       </div>
     </div>
