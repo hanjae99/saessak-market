6fac15b22f36d9310f67ddec8c5f87ea1c076cd7
hanjae <leehanjae99@gmail.com>
2023-11-03 17:41:53 +0900

    더미데이터 크롤링 231103

---
+++ _member__backup.sql
@@ -0,0 +1,6 @@
+INSERT INTO saessak.`member` (member_id,reg_time,update_time,address,email,gender,name,nick_name,password,phone,`role`,user_id) VALUES
+	 (1,'2023-11-03 00:05:21.470874','2023-11-03 00:05:21.470874',NULL,'hanjae@abc.com','MALE','한재','hanjae','$2a$10$zCL7c2FYki5BHo.Kg0bnKuW3EFzWlrLj.KWJSN11Q0chKMM4FL31i','01012341234','USER','hanjae'),
+	 (2,'2023-11-03 00:05:53.817054','2023-11-03 00:05:53.817054',NULL,'sanghyeon@abc.com','MALE','상현','sanghyeon','$2a$10$YWy0ABpoL2UTesjpVIKtTOlC60kcXpD0ENVY51.JM964RHJoneuo2','01023452345','USER','sanghyeon'),
+	 (3,'2023-11-03 00:06:14.238911','2023-11-03 00:06:14.238911',NULL,'gungseo@abc.com','MALE','궁서','gungseo','$2a$10$34YfV.UbKJ5rbDBXwAwBF.HE8sdJo6Jot6giXOXhvkldCDgNRovxm','01034563456','ADMIN','gungseo'),
+	 (4,'2023-11-03 00:06:35.435014','2023-11-03 00:06:35.435014',NULL,'jin@abc.com','MALE','진','jin','$2a$10$d/.mH0PJ8rYGkbzDWFfPveCwpBviSncWQfOZaTVMGU.OVfZi.JThy','01045674567','USER','jin'),
+	 (5,'2023-11-03 00:06:59.271946','2023-11-03 00:06:59.271946',NULL,'sangmo@abc.com','MALE','상모','sangmo','$2a$10$4akaLUdfv5I08jimQ5mzzeV5sb.gA56yK9eD3epzTI01taZ4FaRsq','01056785678','USER','sangmo');
---
+++ saessak-back/src/test/java/com/saessak/crawling/ProductCrawlServiceTest.java
@@ -0,0 +1,24 @@
+package com.saessak.crawling;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+//@Transactional
+class ProductCrawlServiceTest {
+
+    @Autowired
+    ProductCrawlService productCrawlService;
+
+    @Test
+    @DisplayName("상품 크롤링 테스트")
+    public void crawlTest(){
+        productCrawlService.insert();
+    }
+}
